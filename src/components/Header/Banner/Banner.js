59dd6e9293b508fd95f1bfd712afcaff71abe5ac
iamwendellbalagot <johnwendellbalagot11111@gmail.com>
2020-10-28 01:16:27 +0800

    Added Trailer on BANNER

--- src/components/Header/Banner/Banner.js
+++ src/components/Header/Banner/Banner.js
@@ -4,38 +4,57 @@ import './Banner.css';
 import requests from '../../../request/request';
 import axios from '../../../axios/axios';
 
+import movieTrailer from 'movie-trailer';
+import ReactPlayer from 'react-player';
+
 function Banner() {
     const [bannerFeatured, setBannerFeatured] = useState(null);
+    const [playing, setPlaying] = useState('');
+    
     useEffect(() =>{
         axios.get(requests.urlTrending)
         .then(res =>{
             console.log(res)
             const randomNum = Math.floor(Math.random() * res.data.results.length) + 1;
             setBannerFeatured(res.data.results[randomNum])
+            
         })
     }, [])
+
+
+    const playTrailer = (title) => {
+        movieTrailer(title)
+        .then(res =>{
+            playing?setPlaying(''):setPlaying(<ReactPlayer url={res} width={'100%'} height={500} playing={true} />)
+        })
+        .catch(err => console.log(err))
+    }
 
     const truncate = (str, n) =>{
         return (str?.length> n)? str.substr(0, n-1) + '...' : str
     }
 
     return (
-        <div className = 'banner'
-            style = {{
-                backgroundImage: `url('https://image.tmdb.org/t/p/original${bannerFeatured?.backdrop_path}')`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center center'
-            }}>
-            <div className='banner__contents'>
-                <h1>{bannerFeatured?.name || bannerFeatured?.title || bannerFeatured?.original_title}</h1>
-                <div className='banner__buttons'>
-                    <button>Play</button>
-                    <button>My List</button>
+        <React.Fragment>
+            <div className = 'banner'
+                style = {{
+                    backgroundImage: `url('https://image.tmdb.org/t/p/original${bannerFeatured?.backdrop_path}')`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center center'
+                }}>
+                <div className='banner__contents'>
+                    <h1>{bannerFeatured?.name || bannerFeatured?.title || bannerFeatured?.original_title}</h1>
+                    <div className='banner__buttons'>
+                        <button onClick = {() => playTrailer(bannerFeatured?.name || bannerFeatured?.title || bannerFeatured?.original_title)}>Play</button>
+                        <button>My List</button>
+                    </div>
+                    <p>{truncate(bannerFeatured?.overview, 200)}</p>
                 </div>
-                <p>{truncate(bannerFeatured?.overview, 200)}</p>
+                <div className='banner--fadeBottom '></div>
             </div>
-            <div className='banner--fadeBottom '></div>
-        </div>
+            {playing}
+        </React.Fragment>
+        
     )
 }
 
