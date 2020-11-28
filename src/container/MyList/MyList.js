06a718bbd365b67c109123ac0c71224d758373aa
iamwendellbalagot <johnwendellbalagot11111@gmail.com>
2020-11-29 05:34:45 +0800

    Added movies on List

--- src/container/MyList/MyList.js
+++ src/container/MyList/MyList.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './MyList.css';
-import Navigaition from '../../components/Navigation/Navigation';
 import {useSelector} from 'react-redux';
-import { getUser } from '../../reduxSlices/userSlice';
+import { getUser, getMovies } from '../../reduxSlices/userSlice';
 import { db, auth } from '../../firebase';
+
+const imgURL = 'https://image.tmdb.org/t/p/original';
 
 const MyList = () => {
     const user = useSelector(getUser);
+    const movies = useSelector(getMovies);
 
     const handleLogout = () => {
         auth.signOut()
@@ -18,7 +20,13 @@ const MyList = () => {
                 <span onClick={handleLogout} >Home</span>
             </div>
             <div className="mylist__movies">
-
+                {movies? movies.map(movie => (
+                    <img 
+                        src={`${imgURL}${movie?.movie.poster_path}`} 
+                        alt={movie.movie.title ? movie.movie.title : movie.movie.name} 
+                        key ={movie.movie.id} 
+                     />
+                )) : null}
             </div>
         </div>
     )
